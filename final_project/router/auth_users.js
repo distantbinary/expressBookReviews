7c92dea25ba4e251818def64e35f5e337861dea5
sp <staticpogona@gmail.com>
2023-04-09 16:17:40 -0400

    minor changes, still working on task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,18 +11,32 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    let userLength=users.length;
+    for (let num=0; num<userLength; user++){
+        if (users[num]["username"]===username && users[num]["password"]===password){
+            //username and password matched!
+        }
+        else {
+            //user name and/or password don't match!
+        }
+    }
 }
 
 
 
 
 
-
+let userLength=users.length;
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+const username = req.body.username;
+const password = req.body.password;
+res.send(users.length.toString());
+//   return res.status(404).json({message: "No username or password provided."});
+//   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,7 +15,6 @@ const doesExist = (username)=>{
     }
 }
 
-
 public_users.post("/register", (req,res) => {//register new user
     const username = req.body.username;
     const password = req.body.password;
@@ -34,26 +33,18 @@ public_users.post("/register", (req,res) => {//register new user
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   res.send(JSON.stringify(books,null,4));
-
-  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-
   const isbn = req.params.isbn;
   res.send(books[isbn]);
-
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-
   const author = req.params.author;
   const result= {};
 
@@ -67,8 +58,6 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-
   const title = req.params.title;
   const result= {};
 
@@ -82,8 +71,6 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-
   const isbn = req.params.isbn;
   res.send(books[isbn]);
 });
