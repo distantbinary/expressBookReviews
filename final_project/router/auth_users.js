e3d269421e853a104970e4ffa2bbe5be85fd3198
sp <staticpogona@gmail.com>
2023-04-10 03:05:42 -0400

    added session auth, still need testing

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,43 +6,56 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    if (typeof username === 'undefined' || username.trim() === '') {
+        // username is undefined or empty string, return false
+        return false;
+    }
+    else{
+        return true;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
     let userLength=users.length;
-    for (let num=0; num<userLength; user++){
+    for (let num=0; num<userLength; num++){
         if (users[num]["username"]===username && users[num]["password"]===password){
-            //username and password matched!
-        }
-        else {
-            //user name and/or password don't match!
+            return true;
         }
     }
+    return false;
 }
-
-
-
-
-
-let userLength=users.length;
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
 
-const username = req.body.username;
-const password = req.body.password;
-res.send(users.length.toString());
-//   return res.status(404).json({message: "No username or password provided."});
-//   return res.status(300).json({message: "Yet to be implemented"});
+    if (!isValid(username)){
+        return res.status(404).json({message: "Username is not valid."});
+    }
+
+    if (authenticatedUser(username, password)){
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 });
+    
+        req.session.authorization = {
+            accessToken,username
+        }
+        return res.status(200).send("User successfully logged in");
+    }
+    else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(req.session.authorization["username"], null, 4));
 });
 
 module.exports.authenticated = regd_users;
