12243129813c769704263ec6bdd08346e1beaf3a
sp <staticpogona@gmail.com>
2023-04-10 12:39:39 -0400

    minor adjustment to get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -87,7 +87,7 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    res.send(books[isbn]['reviews']);
 });
 
 module.exports.general = public_users;
